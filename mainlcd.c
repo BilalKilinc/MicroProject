d72005383c3a6d36ff584264f60e9adf6dc548cd
atesibra <101096064+atesibra@users.noreply.github.com>
2024-06-25 09:04:24 +0300

    Add files via upload

---
+++ mainlcd.c
@@ -0,0 +1,20 @@
+#include <xc.h>
+#include "config.h"
+#include "I2C_LCD.h"
+
+void main(void) {
+
+    I2C_Master_Init();
+    LCD_Init(0x4E); // Initialize LCD module with I2C address = 0x4E
+
+    LCD_Set_Cursor(1, 1);
+    LCD_Write_String(" deneme");
+    LCD_Set_Cursor(2, 1);
+    LCD_Write_String(" basarili");
+
+    while(1)
+    {
+
+        }
+    return;
+}
