39346b40bd457855e8ff060d1d02fd1484333b50
BilalKilinc <yusufbilalkilinc@hotmail.com>
2024-06-25 10:38:34 +0300

    Update mainlcd.c

--- mainlcd.c
+++ mainlcd.c
@@ -1,5 +1,16 @@
+// CONFIG
+#pragma config FOSC = HS// Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = ON // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOREN = OFF // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF  // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit
+                          // (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF  // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF  // Flash Program Memory Write Enable bits
+                          // (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF   // Flash Program Memory Code Protection bit (Code protection off)
+
 #include <xc.h>
-#include "config.h"
 #include "I2C_LCD.h"
 
 void main(void) {
